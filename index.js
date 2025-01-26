c69fe96c2cf91c04ebe42abb397892ca59c3f9e7
lizkisssss <lizasalamakha2010@gmail.com>
2025-01-26 12:57:56 +0200

    homework-one-js

---
+++ index.js
@@ -0,0 +1,29 @@
+const age = 14;
+console.log(age)
+
+const myName = "Lisa";
+console.log(myName)
+
+const isStudent = "false";
+console.log(isStudent)
+
+const myString = "Людина залишилась людиною, навіть після кінця світу";
+console.log(myString)
+
+let myNumber = 5;
+myNumber += 10;
+console.log(myNumber)
+
+const myNull = null;
+console.log(myNull)
+
+const userName = prompt("Введіть своє ім'я");
+alert("Привіт, " + userName + "!");
+console.log("userName", typeof userName);
+
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log("Користувач відповів:", userAnswer);
+
+alert("Увага! Дія може бути небезпечною.");
+const confirmAction = confirm("Ви впевнені, що хочете продовжити?");
+console.log("Користувач підтвердив дію:", confirmAction);
